fc2e73f023fe58f325480db6e0466ce8012149c4
Mardin <54506351+mardin-pop@users.noreply.github.com>
2020-04-17 12:18:53 +0300

    Recognition the shell
    Recognition the shell and create the ".scripts" folder, if does not exist

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
-mkdir ~/.scripts
-cp ./git-extensions/* ~/.scripts 
-echo 'export PATH=$PATH:~/.scripts' >> ~/.zshrc 
-export PATH=$PATH:~/.scripts
+#!/bin/bash
+
+shell_config(){
+  # find the defualt shell
+  defualt_shell=`echo $SHELL | awk -F '/' '{ print $NF }'`
+  case $defualt_shell in
+	  "bash")
+	      echo 'export PATH=$PATH:~/.scripts' >> ~/.bashrc ;;
+	  "zsh")
+	      echo 'export PATH=$PATH:~/.scripts' >> ~/.zshrc  ;;
+	   "fish")
+	      echo 'export PATH=$PATH:~/.scripts' >> ~/.config/fish/config.fish ;;
+     *)
+	      echo '[!] Your shell is not detected by installer script.'
+	      echo -e '\t\t add ~/.script/ folder to PATH env.'
+	   ;;
+}
+
+main(){
+    [ ! -f "$HOME/.scripts" ] && mkdir ~/.scripts
+    cp ./git-extensions/* ~/.scripts/
+    shell_config
+    export PATH=$PATH:~/.scripts
+}
